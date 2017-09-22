3768ad6b9e19fd360fd32027c9614a893120928d
Roman <bondrogeen@gmail.com>
2017-09-22 17:01:06 +0300

    ...

--- main.js
+++ main.js
@@ -113,11 +113,9 @@ adapter.on('message', function (obj) {
         }
     }
 });
-
 adapter.on('ready', function () {
     main();
 });
-
 function setHilink (setid, response ) {
     for (var key in response.response) {
         var val = response.response[key];
@@ -135,7 +133,6 @@ function setHilink (setid, response ) {
         adapter.setState(setid+'.' + key, {val: val, ack: true});
     }
 }
-
 function timeStatus() {
     hilink.smsCount(function( response ){
         setHilink("smscount",response);
@@ -156,7 +153,6 @@ function timeStatus() {
         setHilink("traffic.month",response);
     });
 }
-
 function main() {
     adapter.log.info('config getip: ' + adapter.config.getip);
     adapter.log.info('config trafficInfo: ' + adapter.config.trafficInfo);
